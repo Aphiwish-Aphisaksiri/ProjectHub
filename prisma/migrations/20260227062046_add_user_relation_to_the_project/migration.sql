4411d169778f71b9861701f48a13c19767a6429f
Aphiwish Aphisaksiri <aphiwish.aph@gmail.com>
2026-02-27 13:26:55 +0700

    Add: simple relation of project and user in the db schema

---
+++ prisma/migrations/20260227062046_add_user_relation_to_the_project/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "ProjectVisibility" AS ENUM ('PRIVATE', 'PUBLIC');
+
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "addReadMe" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "visibility" "ProjectVisibility" NOT NULL DEFAULT 'PRIVATE';
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "avatarUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
