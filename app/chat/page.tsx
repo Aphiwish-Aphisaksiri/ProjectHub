90e05683e43056dd7b2141f69ff7f75b0e4a384b
Aphiwish Aphisaksiri <aphiwish.aph@gmail.com>
2026-03-04 12:56:04 +0700

    Fix: h-screen with navbar makes the webpage longer than it should

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-black text-white flex flex-col items-center justify-center h-screen text-3xl"> 
+    <div className="bg-black text-white flex flex-col items-center justify-center h-full text-3xl"> 
         <h1 className="text-3xl font-bold">Chat Page</h1>
         <h2 className="text-lg mt-4">This is where the chatbot interface will be displayed.</h2>
     </div>
--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-black text-white flex flex-col items-center justify-center h-screen text-3xl"> 
+    <div className="bg-black text-white flex flex-col items-center justify-center h-full text-3xl"> 
         <h1 className="text-3xl font-bold">Tasks Page</h1>
         <h2 className="text-lg mt-4">This is where your tasks will be displayed.</h2>
     </div>
