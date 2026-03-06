2aff4f2d8920044e435a96779992301b455d4a45
Aphiwish Aphisaksiri <aphiwish.aph@gmail.com>
2026-03-06 14:29:04 +0700

    Feat: Add signout button

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,13 +1,19 @@
+'use client';
+
 import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 export default function UserPage() {
-  return (
-    <div className="bg-black text-white flex flex-col items-center justify-center h-full text-3xl">
-        <h1 className="text-3xl font-bold">User Page</h1>
-        <h2 className="text-lg mt-4">This is where your profile information will be displayed.</h2>
-        <Link href="/user/signin" className="mt-6 px-4 py-2 bg-blue-600 rounded hover:bg-blue-700 transition">
-            Sign In
-        </Link>
-    </div>
+    return (
+        <div className="bg-black text-white flex flex-col items-center justify-center h-full text-3xl">
+            <h1 className="text-3xl font-bold">User Page</h1>
+            <h2 className="text-lg mt-4">This is where your profile information will be displayed.</h2>
+            <Link href="/user/signin" className="mt-6 px-4 py-2 bg-blue-600 rounded hover:bg-blue-700 transition">
+                Sign In
+            </Link>
+            <button onClick={() => signOut()} className="mt-4 px-4 py-2 bg-red-600 rounded hover:bg-red-700 transition">
+                Sign Out
+            </button>
+        </div>
     );
 }
