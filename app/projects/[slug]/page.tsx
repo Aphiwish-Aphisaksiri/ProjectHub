94f1535d889f0d30dde00f3448d529e44d4c58ef
Aphiwish Aphisaksiri <aphiwish.aph@gmail.com>
2026-03-14 13:50:34 +0700

    Feat: Landing page for project
    - the url will be /projects/[slug].
    - added 2 Button link to /projects/[slug]/tasks/new and /projects/[slug]/notes/new

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,6 +1,7 @@
 // app/projects/[slug]/page.tsx
 
 import { prisma } from "@/lib/prisma";
+import Link from "next/link";
 
 
 export default async function ProjectPage({ params }: { params: Promise<{ slug: string }> }) {
@@ -9,9 +10,31 @@ export default async function ProjectPage({ params }: { params: Promise<{ slug: 
         where: { slug }
     })
     return (
-        <div>
-            <h1>{project?.title}</h1>
-            <p>{project?.description}</p>
+        <div className="bg-primary text-offwhite flex flex-col items-start justify-start h-full max-h-full p-12">
+            <div className="topBar flex flex-row items-center justify-between w-full mb-8">
+                <div className="projectInfo flex flex-col items-start justify-start gap-2">
+                    <h1 className="text-4xl font-bold">{project?.title}</h1>
+                    <p className="text-xl font-semibold">{project?.description}</p>
+                </div>
+                <div className="projectActions flex flex-row items-center justify-start gap-4 w-75">
+                    <Link
+                        href={`/projects/${slug}/tasks/new`}
+                        className="btn-createTask flex flex-row items-center justify-center gap-2.5 px-1.25 py-2 w-full h-full
+                        text-offwhite text-[16px] font-bold
+                        rounded-md bg-green hover:bg-green/50">
+                        Create Task
+                    </Link>
+                    <Link
+                        href={`/projects/${slug}/notes/new`}
+                        className="btn-createNote flex flex-row items-center justify-center gap-2.5 px-1.25 py-2 w-full h-full
+                        text-offwhite text-[16px] font-bold
+                        rounded-md bg-green hover:bg-green/50">
+                        Create Note
+                    </Link>
+                </div>
+            </div>
+
+            <div>WIP: This should be a table</div>
         </div>
     )
 }
