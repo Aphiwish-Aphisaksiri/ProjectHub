ea9613f41214248f23ded62e53657ed15acf026d
Aphiwish <aphiwish.aph@gmail.com>
2026-03-22 14:54:39 +0700

    Feat: Tests for project title exists

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('.', import.meta.url))
+    }
+  },
   test: {
     environment: 'jsdom',    // simulates a browser environment
     globals: true,
